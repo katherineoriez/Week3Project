d96a266a0ebe0230993261f7f504c0bc54ca4846
The Most Awesome of Awesome <KatherineOriez@mwc-091020.dhcp.missouri.edu>
2015-02-09 13:33:24 -0600

    week 3 hw

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+	getData();
 });
 
+function getData() { 
+	$.getJSON("js/JayBruceStats.json", function(data) {
+		playerData = data;
+		drawChart();
+		});
+}
 
 
+function drawChart (){
+	
+	$(".player-name".html).html(playerData.name);
+	
+	$(".chart").html("<h1>"+playerData.name+"</h1>");
+
+	$(".chart").append("<p>"+playerData.club+"</p>");
+
+	$.each(playerData.stats, function(i,item) {
+		var width = item.HR * 10;
+		$(".chart").append(
+			"<div class=col-md-12 bar-container'>"+
+			"<p><div class='bar' style='width: "+width+"px'></div>"+
+			"<div class='text'>Year: "+item.year+" Home Runs: " +item.HR+ "</div>"+
+			"</div>"
+		);
+	});
+}
 
 
 
